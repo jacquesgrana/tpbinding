d3c394fdac1b1c91617262f2483fc383b957288c
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-08-31 13:09:58 +0200

    tp ok + ajout affichage poster, ratings, boxoffice

--- src/app/site/films/directives/list.directive.ts
+++ src/app/site/films/directives/list.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from '@angular/core';
-
+import { Directive, Input, ElementRef } from '@angular/core';
 @Directive({
-  selector: '[appList]'
+  selector: '[movieList]'
 })
 export class ListDirective {
-
-  constructor() { }
-
+  private _list!: Object;
+  constructor(private el: ElementRef) { }
+  @Input()
+  set movieList(movie: any) {
+    let temp = `<img class=""
+    style="width:33%; margin:auto"
+    src="${movie.Poster}"
+    alt="poster du film ${movie.Title}">`;
+    temp += `<a class="list-group-item list-group-item-action">`;
+    temp +=
+      `Titre : ${movie.Title} / Année : ${movie.Year} / Réalisateur : ${movie.Director}`; // movie.Poster Ratings BoxOffice
+    temp += ` / Ratings : ${movie.Ratings[0].Value} / BoxOffice : ${movie.BoxOffice}`;
+    temp += '</a>';
+    this.el.nativeElement.innerHTML = temp;
+  }
 }
